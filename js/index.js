78d353dea1194cdb8ba879362e54acd862450d12
Afonso Oliveira <afonso.noliveira29@gmail.com>
2020-03-23 15:11:10 -0300

    Revert "Add fat percentual message"
    This reverts commit 5bc9b545196356b0efac34109b67cbbecb1c8cbe.

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ function iniciarCalculo() {
   let altura = document.getElementById("height").value.replace(",",".");
   altura = parseFloat(altura);
   
-  // pega o valor do input da massa e transforma a 
+  // pega o valor do input da massa
   let massa = document.getElementById("kilogram").value.replace(",",".");
   massa = parseFloat(massa);
 
@@ -12,9 +12,8 @@ function iniciarCalculo() {
   totalImc = calcularImc(altura,massa)
 
   // Joga o valor do totalImc dentro do input IMC
-  document.getElementById("imc").value = totalImc.toFixed(2).replace(".",",");
-
-  mostrarMensagemPeso(totalImc);
+  document.getElementById("imc").value = totalImc.toFixed(2);
+  
 }
 
 // Função responsável por executar o calculo do IMC
@@ -30,40 +29,4 @@ function limpar() {
   document.getElementById("kilogram").value = '';
   // pega o valor do input do imc e limpa o campo
   document.getElementById("imc").value = '';
-}
-
-function mostrarMensagemPeso(imc) {
-  const divCalculator = document.querySelector('.calculator')
-  let msg = ''
-
-  if(imc < 18.5) {
-    msg = document.createElement('p');
-    msg.setAttribute('class', 'type-of-mass')
-    msg.innerHTML= 'Menor que 18.5: Magreza';
-    divCalculator.appendChild(msg);
-
-  } else if ( imc > 18.5 && imc < 24.9) {
-    msg = document.createElement('p');
-    msg.setAttribute('class', 'type-of-mass')
-    msg.innerHTML= 'Entre 18,5 E 24,9: Normal';
-    divCalculator.appendChild(msg);
-
-  } else if(imc > 25 && imc < 29.9) {
-    msg = document.createElement('p');
-    msg.setAttribute('class', 'type-of-mass')
-    msg.innerHTML= 'Entre 25,0 E 29,9: Sobrepeso';
-    divCalculator.appendChild(msg);
-
-  } else if(imc > 30 && imc < 39.9) {
-    msg = document.createElement('p');
-    msg.setAttribute('class', 'type-of-mass')
-    msg.innerHTML= 'Entre 30,0 E 39,9: Obesidade';
-    divCalculator.appendChild(msg);
-
-  } else if(imc > 40) {
-    msg = document.createElement('p');
-    msg.setAttribute('class', 'type-of-mass')
-    msg.innerHTML= 'Maior que 40,0: Obesidade Grave';
-    divCalculator.appendChild(msg);
-  }
 }
